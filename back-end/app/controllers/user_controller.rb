2476e2b7382b9f6399c31bce0933343e9d4c2561
Shobhit Shekhar <shbhtshkhr147@gmail.com>
2017-03-24 03:00:00 +0530

    user controller updated

--- back-end/app/controllers/user_controller.rb
+++ back-end/app/controllers/user_controller.rb
@@ -1,3 +1,21 @@
 class UserController < ApplicationController
-  
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :contact, :email,
+                                   :phone_no_verified, :aadhar_verified)
+    end
 end
