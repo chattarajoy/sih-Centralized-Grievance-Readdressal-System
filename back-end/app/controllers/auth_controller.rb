592006a868b3a572ba1e0d4cf865eca8566c34aa
Joy Lal Chattaraj <joylal4896@gmail.com>
2017-03-24 02:05:04 +0530

    Users can now login

---
+++ back-end/app/controllers/auth_controller.rb
@@ -0,0 +1,36 @@
+class AuthController < ApplicationController
+
+
+  def user_login
+
+    if params[:password] && params[:email]
+
+        user = User.where(email: params[:email]).first
+
+        if user && user.authenticate(params[:password])
+            api_key = ApiKey.new
+            api_key.user_id = user.id
+            api_key.save
+            token = api_key.secret_key
+
+            render json: {status: "success", access_token: token}
+        else
+          notice = "Invalid email or password"
+        end
+    else
+        notice = "Invalid Params"
+    end
+    render json: {status: "error", notice: notice} unless token
+
+  end
+
+  def office_login
+
+  end
+
+  def logout
+
+  end
+
+
+end
