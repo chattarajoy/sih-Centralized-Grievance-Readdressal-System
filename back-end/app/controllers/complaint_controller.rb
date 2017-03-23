108d4275b97dc5dc6f71d8d160d482e3af10d21b
Shobhit Shekhar <shbhtshkhr147@gmail.com>
2017-03-24 02:09:32 +0530

    new, create, update added to complaint controller

--- back-end/app/controllers/complaint_controller.rb
+++ back-end/app/controllers/complaint_controller.rb
@@ -1,2 +1,12 @@
 class ComplaintController < ApplicationController
+  def new
+    @complaint = Complaint.new
+  end
+
+  def create
+    @complaint = Complaint.new(complaint_params)
+  end
+
+  def update
+  end
 end
