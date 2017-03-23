bd528e361da1a60ec4e6cae55ca911b3af8dbce1
Joy Lal Chattaraj <joylal4896@gmail.com>
2017-03-23 09:46:21 +0530

    common routes added

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  match ':controller(/:action(/:id))' , via => [get, post]
+
 end
