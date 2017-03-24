7b8308e284aab56db9b72a3194a4c099ea849103
Joy Lal Chattaraj <joylal4896@gmail.com>
2017-03-24 13:44:34 +0530

    Accidental removal of migrations

--- back-end/db/migrate/20170323211320_add_user_token_to_complaints.rb
+++
@@ -1,5 +0,0 @@
-class AddUserTokenToComplaints < ActiveRecord::Migration[5.0]
-  def change
-    add_column :complaints, :user_token, :string
-  end
-end
--- back-end/db/migrate/20170323211523_add_user_type_to_complaints.rb
+++
@@ -1,5 +0,0 @@
-class AddUserTypeToComplaints < ActiveRecord::Migration[5.0]
-  def change
-    add_column :complaints, :user_type, :string
-  end
-end
