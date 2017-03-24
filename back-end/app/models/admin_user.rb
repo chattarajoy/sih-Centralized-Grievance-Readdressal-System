448d0eb0ae2b3bc13d520e3d4bdf73444b3d376f
hjobs <himanshujh46@gmail.com>
2017-03-24 15:30:07 +0530

    Validation of email and name for admin user

--- back-end/app/models/admin_user.rb
+++ back-end/app/models/admin_user.rb
@@ -1,7 +1,13 @@
 class AdminUser < ApplicationRecord
 	has_many :complaint_updates
 	has_one :municipal_office
-
+	before_save { self.email = email.downcase }
+	validates :name, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+    	              format: { with: VALID_EMAIL_REGEX },
+        	          uniqueness: { case_sensitive: false }
+    validates :phone, presence: true
   	has_secure_password
 
 end
