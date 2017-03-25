10349e79b48e87c191d9316e49320287bb6acffa
hjobs <himanshujh46@gmail.com>
2017-03-26 04:26:46 +0530

    contact us mailer created with name my_mailer

--- back-end/config/environments/development.rb
+++ back-end/config/environments/development.rb
@@ -26,8 +26,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -45,11 +47,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # SMTP settings for gmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => truse
+    :address => "email-smtp.us-east-1.amazonaws.com",
+    :port => 587,
+    :user_name => "AKIAI7QITEB42CFVW34Q", #Your SMTP user
+    :password => "AoKFagtDRA4b2SxfL43Gl0VnzstIgx+dRxiT73tAt7Ou", #Your SMTP password
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
 end
