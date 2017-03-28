9be5e4afd3bca54d8cc56b51e838a5e3f3ebd5e9
Shobhit Shekhar <shbhtshkhr147@gmail.com>
2017-03-29 00:54:30 +0530

    Galti hata diye

--- back-end/app/controllers/auth_controller.rb
+++ back-end/app/controllers/auth_controller.rb
@@ -91,45 +91,4 @@ class AuthController < ApplicationController
     end
   end
 
-  def reset_password
-    if params[:access_token] && params[:secret_key] && params[:password]
-      user_link = PasswordResetLink.where(access_token: params[:access_token],
-                secret_key: params[:secret_key]).first
-      if user_link
-        user = AdminUser.find(user_link.user_id)
-        user.password = params[:password]
-        if user.save
-          render json: {status: "success", message: "password changed"}
-        else
-          error_message = user.errors.full_messages
-        end
-      else
-        error_message = "User not found"
-      end
-    else
-      error_message = "Invalid Parameters"
-    end
-    render json: {status: "error", error_message: error_message}
-  end
-
-  def request_password_reset
-    if params[:email]
-      user = AdminUser.where(email: params[:email]).first
-      if user
-        password_reset_link = PasswordResetLink.new(user_id: user.id)
-        if password_reset_link.save
-          #todo: send email
-          render json: {status: "success", message: "Reset mail sent"} and return
-        else
-          error_message = password_reset_link.errors.full_messages
-        end
-      else
-        error_message = "User not found"
-      end
-    else
-      error_message = "Invalid parameters"
-    end
-    render json: {status: "error", error_message: error_message}
-  end
-
 end
