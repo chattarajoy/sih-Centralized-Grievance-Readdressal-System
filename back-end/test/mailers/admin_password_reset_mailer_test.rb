328add41a6ad0b9a40d3fbabdbfa3feb75791be2
Shobhit Shekhar <shbhtshkhr147@gmail.com>
2017-03-29 00:38:06 +0530

    mailer for admin created

---
+++ back-end/test/mailers/admin_password_reset_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AdminPasswordResetMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ back-end/test/mailers/previews/admin_password_reset_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/admin_password_reset_mailer
+class AdminPasswordResetMailerPreview < ActionMailer::Preview
+
+end
