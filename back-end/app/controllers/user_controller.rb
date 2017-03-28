72c7578e2d9e2ae49e2950a5d3f72795ce5e1c71
hjobs <himanshujh46@gmail.com>
2017-03-28 23:41:54 +0530

    reset password for users implemented

--- back-end/app/controllers/user_controller.rb
+++ back-end/app/controllers/user_controller.rb
@@ -50,4 +50,45 @@ class UserController < ApplicationController
     end
   end
 
+  def reset_password
+    if params[:access_token ] && params[:secret_key] && params[:password]
+      user_link = PasswordResetLink.where(access_token: params[:access_token] , secret_key: params[:secret_key]).first
+      if user_link
+        user=User.find(user_link.user_id)
+        user.password=params[:password]
+        if user.save
+          render json: {status:"success",message:"Password changed"}
+        else
+          error_message=user.errors.full_messages
+        end
+      else
+        error_message="User not found"
+      end
+    else
+      error_message="Invalid parameters"
+    end
+    render json:{status:"Error",error_message:error_message}
+  end
+
+
+  def request_password_reset
+    if params[:email]
+      user = User.where(email: params[:email]).first
+      if user
+        password_reset_link = PasswordResetLink.new(user_id: user.id)
+        if password_reset_link.save
+          #send email
+          render json: {status:"success",message:"Reset mail sent"} and return
+        else
+          error_message = password_reset_link.errors.full_messages
+        end
+      else
+        error_message="User not found"
+      end
+    else  
+      error_message="Error invalid parameters"
+    end
+    render json:{status:"Error",error_message:error_message}
+  end
+
 end
