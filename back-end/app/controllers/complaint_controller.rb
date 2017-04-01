9ce3a0c38dccac9b0f94f28e2ee0f3e4a8a7d5e3
hjobs <himanshujh46@gmail.com>
2017-04-01 11:43:27 +0530

    sms for complete registration added

--- back-end/app/controllers/complaint_controller.rb
+++ back-end/app/controllers/complaint_controller.rb
@@ -17,10 +17,12 @@ class ComplaintController < ApplicationController
                                 status: "new" ,
                                 priority: "new")
 
+    user_id=get_logged_in_user_id
+    user = User.where(user_id: user_id)
       if complaint.save
         # assign new complaint to respective district office
         assignment_result = register_new_complaint(complaint.id, complaint.state, complaint.district)
-
+        send_sms(user.contact,"Your complaint has been registered. Your complaint id is -" + complaint_id)
         render json: {status: "success", complaint: complaint, message: assignment_result}
       else
         render json: {status: "error", error_message: complaint.errors.full_messages}
@@ -52,10 +54,17 @@ class ComplaintController < ApplicationController
   def assign_complaint #assign complaint to ward officers or a contractor
 
     user = AdminUser.find(get_logged_in_user_id)
-
+    complaint = Complaint.find(params[:id])
+    complaint_status = ComplaintUpdate.where(complaint_id: params[:id]).first
     if user.designation == "District Officer"
+      complaint_status.assigned_to = params[:ward_office_id] 
+      complaint_status.notes = params[:notes]
+
     elsif user.designation == "Ward Officer"
+      ward_officer=ComplaintStatus.where(ward_office_id: params[:id]).first
+
     else
+
     end
 
   end
@@ -73,7 +82,7 @@ private
                                              assigned_to: "District Municipal Officer " + district,
                                              notes: "Auto Assignment by System")
 
-      new_complaint = ComplaintStatus.new(complaint_id: complaint_id,
+      complaint_status = ComplaintStatus.new(complaint_id: complaint_id,
                                        district_office_id: district_office.id)
 
       if complaint_update.save && new_complaint.save
